bc4b43f4f55da8a29a32a477106932a4988f0870
Eric Curtin <ecurtin@redhat.com>
2024-06-28 13:54:40 +0100

    Install script
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cleanup() {
+  rm -rf "$TMP" &
+}
+
+available() {
+  command -v $1 >/dev/null
+}
+
+main() {
+  set -e -o pipefail
+
+  if [ "$(uname -s)" != "Linux" ]; then
+    echo "This script is intended to run on Linux only"
+    return 1
+  fi
+
+  if [ "$EUID" -ne 0 ]; then
+    echo "This script is intended to run as root only"
+    return 2
+  fi
+
+  local bindir
+  for bindir in /usr/local/bin /usr/bin /bin; do
+    echo $PATH | grep -q $bindir && break || continue
+  done
+
+  TMP="$(mktemp -d)"
+  trap cleanup EXIT
+  local from="podman-llm"
+  local url="raw.githubusercontent.com/ericcurtin/podman-llm/main/$FROM"
+  local from="$TMP/$FROM"
+  curl -fsSL -o "$FROM" "https://$url"
+  install -D -m755 "$FROM" "$TO"
+}
+
+main "$@"
+
