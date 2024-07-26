e2717eca62c83558310d53c059ecf592b3beed5b
Eric Curtin <ecurtin@redhat.com>
2024-07-26 13:20:54 +0100

    Install if not available
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- ci.sh
+++ ci.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+available() {
+  command -v "$1" > /dev/null
+}
+
 main() {
   set -ex -o pipefail
+
+  if ! available autopep8 && available apt; then
+    apt install -y python3-autopep8
+  fi
 
   ./podman-build.sh
   curl -fsSL https://raw.githubusercontent.com/containers/ramalama/main/install.sh | sudo bash
