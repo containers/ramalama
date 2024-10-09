d26b783236c3e14fe8d5078af68a801d2486ea06
Daniel J Walsh <dwalsh@redhat.com>
2024-10-09 07:23:34 -0400

    Add -image option to override defaut OCI Image
    By default ramalama attempts to use the best AI image
    based on GPU on the local system. The --image option
    allows users to override the default.
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- ramalama/cli.py
+++ ramalama/cli.py
@@ -51,6 +51,11 @@ def init_cli():
         help="run RamaLama in the default container",
     )
     parser.add_argument(
+        "--image",
+        default=default_image(),
+        help="OCI container image to run with specified AI model",
+    )
+    parser.add_argument(
         "--nocontainer",
         dest="container",
         default=False,
@@ -518,7 +523,7 @@ def run_container(args):
     if os.path.exists("/dev/kfd"):
         conman_args += ["--device", "/dev/kfd"]
 
-    conman_args += [default_image(), "python3", "/usr/bin/ramalama"]
+    conman_args += [args.image, "python3", "/usr/bin/ramalama"]
     conman_args += sys.argv[1:]
     if hasattr(args, "UNRESOLVED_MODEL"):
         index = conman_args.index(args.UNRESOLVED_MODEL)
