cc5d16ae415bc71a4df335782ccd2675ad4cd6ae
Jess Chitas <chitasjessica@gmail.com>
2025-01-03 15:33:24 +0000

    add website and build

---
+++ website/node_modules/gopd/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var GetIntrinsic = require('get-intrinsic');
+
+/** @type {import('.')} */
+var $gOPD = GetIntrinsic('%Object.getOwnPropertyDescriptor%', true);
+
+if ($gOPD) {
+	try {
+		$gOPD([], 'length');
+	} catch (e) {
+		// IE 8 has a broken gOPD
+		$gOPD = null;
+	}
+}
+
+module.exports = $gOPD;
