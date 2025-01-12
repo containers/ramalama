f715a32c909dc02b76ae24dc2e417dbfaacc087f
jim3692 <dim@knp.one>
2025-01-13 00:28:39 +0200

    Enable Flake for MacOS
    Signed-off-by: jim3692 <dim@knp.one>

--- flake.nix
+++ flake.nix
@@ -10,6 +10,8 @@
       supportedSystems = [
         "x86_64-linux"
         "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
 
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
