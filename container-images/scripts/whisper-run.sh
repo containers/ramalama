bf0d770f5250efb84f940d848dfa0a8ee57d8d75
Daniel J Walsh <dwalsh@redhat.com>
2025-02-13 07:23:58 -0500

    Prepare containers to run with ai-lab-recipes
    Add two new scripts llama-server.sh and whisper-server.sh which
    can handle environment variables from the ai-lab-recipes.
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

---
+++ container-images/scripts/whisper-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -n ${MODEL_PATH} ]; then
+    whisper-server \
+	-tr \
+	--model ${MODEL_PATH} \
+	--convert \
+	--host ${HOST:=0.0.0.0} \
+	--port ${PORT:=8001}
+    exit 0
+fi
+
+echo "Please set a MODEL_PATH"
+exit 1
