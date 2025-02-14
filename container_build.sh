4d209b92f42c57010302be5f1aa2bb11c543c408
Daniel J Walsh <dwalsh@redhat.com>
2025-02-14 10:12:03 -0500

    Add entrypoint container images
    Install podman-remote and ramalama so we can use ramalama
    from within a container.
    
    $ podman run --env XDG_RUNTIME_DIR -v $HOME:$HOME -v /run/user:/run/user --userns=keep-id -ti --privileged quay.io/ramalama/ramalama ramalama run granite
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- container_build.sh
+++ container_build.sh
@@ -37,6 +37,22 @@ rm_container_image() {
   fi
 }
 
+add_entrypoint() {
+    containerfile=$(mktemp)
+    cat > ${containerfile} <<EOF
+FROM $2
+ENTRYPOINT [ "/usr/bin/$3.sh" ]
+EOF
+    echo $1 build --no-cache -t "$2-$3" -f ${containerfile} .
+    eval $1 build --no-cache -t "$2-$3" -f ${containerfile} .
+    rm ${containerfile}
+}
+
+add_entrypoints() {
+    add_entrypoint "$1" "$2" "whisper-server"
+    add_entrypoint "$1" "$2" "llama-server"
+}
+
 build() {
   cd "container-images"
   local image_name="${1//container-images\//}"
@@ -55,6 +71,7 @@ build() {
       echo "${conman_build[@]}"
       "${conman_build[@]}"
       "${conman_show_size[@]}"
+      add_entrypoints "${conman[@]}" "$REGISTRY_PATH"/"$image_name"
       rm_container_image
       ;;
     push)
@@ -62,6 +79,7 @@ build() {
       ;;
     multi-arch)
       podman farm build -t "$REGISTRY_PATH"/"$image_name" -f "$image_name"/Containerfile .
+      add_entrypoints "podman farm" "$REGISTRY_PATH"/"$image_name"
       ;;
     *)
       echo "Invalid command: ${2:-}. Use 'build', 'push' or 'multi-arch'."
