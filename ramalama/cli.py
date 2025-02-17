2eab097982f3d3fe5b3f92e2aa5bddf794c119e3
Michael Kesper <mkesper@web.de>
2025-02-17 23:58:24 +0100

    Clarify second dryrun parser argument
    See #684
    
    Signed-off-by: Michael Kesper <mkesper@web.de>

--- ramalama/cli.py
+++ ramalama/cli.py
@@ -168,7 +168,7 @@ The RAMALAMA_IN_CONTAINER environment variable modifies default behaviour.""",
     parser.add_argument(
         "--dryrun", dest="dryrun", action="store_true", help="show container runtime command without executing it"
     )
-    parser.add_argument("--dry-run", dest="dryrun", action="store_true", help=argparse.SUPPRESS)
+    parser.add_argument("--dry-run", dest="dryrun", action="store_true", help=argparse.SUPPRESS)  # Alternative spelling
     parser.add_argument(
         "--engine",
         dest="engine",
