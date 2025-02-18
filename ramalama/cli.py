4cab7b9719cb73956c9ab07fcab511a1f9011e5e
Eric Curtin <ecurtin@redhat.com>
2025-02-18 10:46:10 +0000

    Just one add_argument call for --dryrun/--dry-run
    Just alias a single function call, rather than calling the function
    twice
    
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- ramalama/cli.py
+++ ramalama/cli.py
@@ -166,9 +166,12 @@ def configure_arguments(parser):
 The RAMALAMA_IN_CONTAINER environment variable modifies default behaviour.""",
     )
     parser.add_argument(
-        "--dryrun", dest="dryrun", action="store_true", help="show container runtime command without executing it"
+        "--dryrun",
+        "--dry-run",
+        dest="dryrun",
+        action="store_true",
+        help="show container runtime command without executing it",
     )
-    parser.add_argument("--dry-run", dest="dryrun", action="store_true", help=argparse.SUPPRESS)  # Alternative spelling
     parser.add_argument(
         "--engine",
         dest="engine",
