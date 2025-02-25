d863cc998ebc0340f2341ab4db464472174caff3
Kush Gupta <kushalgupta@gmail.com>
2025-02-25 09:22:14 -0500

    script os agnostic ollama install
    Signed-off-by: Kush Gupta <kushalgupta@gmail.com>

---
+++ .github/scripts/install-ollama.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+os_name=$(uname)
+
+if [[ "$os_name" == "Darwin" ]]; then
+  brew install ollama
+  brew services start ollama
+elif [[ "$os_name" == "Linux" ]]; then
+  curl -L https://ollama.com/download/ollama-linux-amd64.tgz -o ollama-linux-amd64.tgz
+  sudo tar -C /usr -xzf ollama-linux-amd64.tgz
+  sudo useradd -r -s /bin/false -U -m -d /usr/share/ollama ollama
+  sudo usermod -a -G ollama runner
+else
+  echo "Operating system is neither macOS nor Linux"
+fi
