5957153637a87899a6a50cd9d91fb3e1a2c0eb3a
Michael Engel <mengel@redhat.com>
2025-02-26 17:13:37 +0100

    Added unit tests for config functions
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ test/unit/test_config.py
@@ -0,0 +1,91 @@
+import json
+
+import pytest
+
+from ramalama.config import load_config_defaults, load_config_from_env
+
+
+@pytest.mark.parametrize(
+    "env,config,expected",
+    [
+        (
+            {
+                "RAMALAMA_IN_CONTAINER": "true",
+                "RAMALAMA_CONTAINER_ENGINE": "podman",
+                "RAMALAMA_IMAGE": "image",
+                "RAMALAMA_STORE": "~/.local/share/ramalama",
+                "RAMALAMA_TRANSPORT": "ollama",
+            },
+            {},
+            {
+                "container": "true",
+                "engine": "podman",
+                "image": "image",
+                "store": "~/.local/share/ramalama",
+                "transport": "ollama",
+            },
+        ),
+        (
+            {},
+            {
+                "container": "true",
+                "engine": "podman",
+                "image": "image",
+                "store": "~/.local/share/ramalama",
+                "transport": "ollama",
+            },
+            {
+                "container": "true",
+                "engine": "podman",
+                "image": "image",
+                "store": "~/.local/share/ramalama",
+                "transport": "ollama",
+            },
+        ),
+    ],
+)
+def test_load_config_from_env(env, config, expected):
+    load_config_from_env(config, env)
+    assert json.dumps(config, sort_keys=True) == json.dumps(expected, sort_keys=True)
+
+
+@pytest.mark.parametrize(
+    "config,expected",
+    [
+        (
+            {},
+            {
+                "nocontainer": False,
+                "carimage": "registry.access.redhat.com/ubi9-micro:latest",
+                "runtime": "llama.cpp",
+                "ngl": -1,
+                "keep_groups": False,
+                "ctx_size": 2048,
+                "pull": "newer",
+                "temp": "0.8",
+                "host": "0.0.0.0",
+                "port": "8080",
+            },
+        ),
+        (
+            {
+                "nocontainer": True,
+            },
+            {
+                "nocontainer": True,
+                "carimage": "registry.access.redhat.com/ubi9-micro:latest",
+                "runtime": "llama.cpp",
+                "ngl": -1,
+                "keep_groups": False,
+                "ctx_size": 2048,
+                "pull": "newer",
+                "temp": "0.8",
+                "host": "0.0.0.0",
+                "port": "8080",
+            },
+        ),
+    ],
+)
+def test_load_config_defaults(config, expected):
+    load_config_defaults(config)
+    assert json.dumps(config, sort_keys=True) == json.dumps(expected, sort_keys=True)
