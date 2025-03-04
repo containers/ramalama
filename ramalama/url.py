760d7c8eb2b29f39faab88cfdce4e687c808fdf2
Michael Engel <mengel@redhat.com>
2025-03-04 10:59:08 +0100

    Added URL and local file integration with model store
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- ramalama/url.py
+++ ramalama/url.py
@@ -1,7 +1,25 @@
 import os
+import shutil
 
-from ramalama.common import download_file
+from ramalama.common import download_file, generate_sha256
+from ramalama.huggingface import HuggingfaceRepository
 from ramalama.model import Model
+from ramalama.model_store import SnapshotFile
+
+
+class LocalModelFile(SnapshotFile):
+
+    def __init__(
+        self, url, header, hash, name, should_show_progress=False, should_verify_checksum=False, required=True
+    ):
+        super().__init__(url, header, hash, name, should_show_progress, should_verify_checksum, required)
+
+    def download(self, blob_file_path, snapshot_dir):
+        if not os.path.exists(self.url):
+            raise FileNotFoundError(f"No such file: '{self.url}'")
+        # moving from the local location to blob directory so the model store "owns" the data
+        shutil.copy(self.url, blob_file_path)
+        return os.path.relpath(blob_file_path, start=snapshot_dir)
 
 
 class URL(Model):
@@ -20,9 +38,17 @@ class URL(Model):
             model_organization = "/".join(parts[:-2])
             model_tag = parts[-1]
 
+        # handling huggingface specific URLs for more precise identifiers
+        if len(parts) > 2 and HuggingfaceRepository.REGISTRY_URL.endswith(parts[0]) and parts[-2] == "resolve":
+            model_organization = "/".join(parts[:-2])
+            model_tag = parts[-1]
+
         return model_name, model_tag, model_organization
 
     def pull(self, args):
+        if self.store is not None:
+            return self._pull_with_model_store()
+
         model_path = self.model_path(args)
         directory_path = os.path.join(args.store, "repos", self.type, self.directory, self.filename)
         os.makedirs(directory_path, exist_ok=True)
@@ -49,3 +75,37 @@ class URL(Model):
             os.symlink(relative_target_path, model_path)
 
         return model_path
+
+    def _pull_with_model_store(self):
+        name, tag, _ = self.extract_model_identifiers()
+        hash, _, all = self.store.get_cached_files(tag)
+        if all:
+            return self.store.get_snapshot_file_path(hash, name)
+
+        files: list[SnapshotFile] = []
+        snapshot_hash = generate_sha256(name)
+        if self.type == "file":
+            files.append(
+                LocalModelFile(
+                    url=self.model,  # model contains the full path here
+                    header={},
+                    hash=snapshot_hash,
+                    name=name,
+                    required=True,
+                )
+            )
+        else:
+            files.append(
+                SnapshotFile(
+                    url=f"{self.type}://{self.model}",
+                    header={},
+                    hash=snapshot_hash,
+                    name=name,
+                    should_show_progress=True,
+                    required=True,
+                )
+            )
+
+        self.store.new_snapshot(tag, snapshot_hash, files)
+
+        return self.store.get_snapshot_file_path(snapshot_hash, name)
