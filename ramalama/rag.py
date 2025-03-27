43602c3dd9837853ab5990b5d345fd17fa1d21ce
Brian <bmahabir@bu.edu>
2025-03-26 21:13:08 -0400

    fixed rag argument error and doc2rag error
    Signed-off-by: Brian <bmahabir@bu.edu>

--- ramalama/rag.py
+++ ramalama/rag.py
@@ -90,9 +90,8 @@ COPY {src} /vector.db
         exec_args += ["doc2rag", "/output", "/docs/"]
         try:
             run_cmd(exec_args, debug=args.debug)
-            print(self.build(vectordb.name, self.target, ragdb.name, vectordb.name, args))
+            print(self.build(vectordb.name, self.target, ragdb.name, args))
         except subprocess.CalledProcessError as e:
             raise e
         finally:
             shutil.rmtree(ragdb.name, ignore_errors=True)
-            shutil.rmtree(vectordb.name, ignore_errors=True)
