03ffa948c497d4163f6872d2842993a44d26a14b
Ian Eaves <ian.k.eaves@gmail.com>
2025-06-19 15:32:56 -0500

    fix for nargs being list
    Signed-off-by: Ian Eaves <ian.k.eaves@gmail.com>

--- ramalama/chat.py
+++ ramalama/chat.py
@@ -194,7 +194,10 @@ def alarm_handler(signum, frame):
 
 def chat(args):
     if inputs := getattr(args, "input", None):
-        args.ARGS.append(FileUpLoader(inputs).load())
+        if args.ARGS is not None:
+            args.ARGS.append(FileUpLoader(inputs).load())
+        else:
+            args.ARGS = FileUpLoader(inputs).load()
 
     if args.dryrun:
         prompt = dry_run(args.ARGS)
