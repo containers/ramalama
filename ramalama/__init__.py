5f3fe6a8ba93e1ede7c29a28b67b91d6be564e40
Ian Eaves <ian.k.eaves@gmail.com>
2025-10-09 09:58:33 -0500

    lint
    Signed-off-by: Ian Eaves <ian.k.eaves@gmail.com>

--- ramalama/__init__.py
+++ ramalama/__init__.py
@@ -5,7 +5,6 @@ import sys
 from ramalama.cli import HelpException, init_cli, print_version
 from ramalama.common import perror
 
-
 assert sys.version_info >= (3, 10), "Python 3.10 or greater is required."
 
 __all__ = ["perror", "init_cli", "print_version", "HelpException"]
