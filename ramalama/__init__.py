f894a826fa4285274eb99d2827788c046ec9d7e1
Ian Eaves <ian.k.eaves@gmail.com>
2025-10-09 09:19:31 -0500

    lint
    Signed-off-by: Ian Eaves <ian.k.eaves@gmail.com>

--- ramalama/__init__.py
+++ ramalama/__init__.py
@@ -1,11 +1,10 @@
 """ramalama client module."""
 
-import os
 import sys
 
 from ramalama.cli import HelpException, init_cli, print_version
-from ramalama.common import apple_vm, perror
-from ramalama.config import CONFIG
+from ramalama.common import perror
+
 
 assert sys.version_info >= (3, 10), "Python 3.10 or greater is required."
 
