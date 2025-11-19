194ca157257a73038a1f1c51ca1eb6061a7d49f6
Mike Bonnet <mikeb@redhat.com>
2025-11-18 17:33:55 -0800

    rag: update to Fedora 43
    Update the -rag images to Fedora 43.
    
    Fedora 43 uses python 3.14 as the default version, but not all dependencies
    have been compiled for 3.14 yet, so install and use python 3.13.
    
    Signed-off-by: Mike Bonnet <mikeb@redhat.com>

--- container-images/scripts/build_rag.sh
+++ container-images/scripts/build_rag.sh
@@ -2,36 +2,39 @@
 
 install_pkgs() {
     local pkgs=("git-core" "libglvnd-glx")
+    local default_python="python3.13"
 
     if [ "$backend" = "cu128" ]; then
-        pkgs+=("python3.12" "python3.12-devel" "libcudnn9-devel-cuda-12" "libcusparselt0" "cuda-cupti-12-*")
-        ln -sf python3.12 /usr/bin/python3
+        default_python="python3.12"
+        pkgs+=("libcudnn9-devel-cuda-12" "libcusparselt0" "cuda-cupti-12-*")
     elif [ "$backend" = "rocm6.3" ]; then
-        pkgs+=("python3" "python3-devel" "uv" "rocm-core" "hipblas" "rocblas" "rocm-hip")
+        pkgs+=("uv" "rocm-core" "hipblas" "rocblas" "rocm-hip")
     elif [ "$backend" = "xpu" ]; then
-        pkgs+=("python3" "python3-devel" "uv" "oneapi-level-zero" "intel-level-zero")
+        pkgs+=("uv" "oneapi-level-zero" "intel-level-zero")
     elif [ "$backend" = "cpu" ]; then
-        pkgs+=("python3" "python3-devel" "uv")
+        pkgs+=("uv")
     else
         echo "Unsupported torch backend: $backend"
         exit 1
     fi
+    pkgs+=("$default_python" "${default_python}-devel")
 
     dnf -y --nodocs --setopt=install_weak_deps=false install "${pkgs[@]}"
     dnf -y clean all
-}
 
-install_requirements() {
     if [ "$backend" = "cu128" ]; then
         curl -LsSf https://astral.sh/uv/install.sh | sh
         # shellcheck disable=SC1091
         source /root/.local/bin/env
     fi
-    uv venv "$VIRTUAL_ENV"
+    uv venv --python "/usr/bin/$default_python" "$VIRTUAL_ENV"
+}
+
+install_requirements() {
     if [ -f "/var/tmp/requirements-rag-$backend-$(uname -m).txt" ]; then
-        uv pip sync "/var/tmp/requirements-rag-$backend-$(uname -m).txt"
+        uv pip install -r "/var/tmp/requirements-rag-$backend-$(uname -m).txt"
     else
-        uv pip sync "/var/tmp/requirements-rag-$backend.txt"
+        uv pip install -r "/var/tmp/requirements-rag-$backend.txt"
     fi
 }
 
