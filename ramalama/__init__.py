7e036ff794a58336edac58e2df03f9aa57ef0684
Ian Eaves <ian.k.eaves@gmail.com>
2025-12-02 11:16:44 -0600

    fixed init
    Signed-off-by: Ian Eaves <ian.k.eaves@gmail.com>

--- ramalama/__init__.py
+++ ramalama/__init__.py
@@ -1,11 +1,11 @@
-"""Chat provider abstractions."""
+"""ramalama client module."""
 
-from ramalama.chat_providers import api_providers, openai
-from ramalama.chat_providers.base import (
-    ChatProvider,
-    ChatProviderError,
-    ChatRequestOptions,
-    ChatStreamEvent,
-)
+import sys
 
-__all__ = ["ChatProvider", "ChatProviderError", "ChatRequestOptions", "ChatStreamEvent", "openai", "api_providers"]
+from ramalama import cli
+from ramalama.cli import HelpException, init_cli, print_version
+from ramalama.common import perror
+
+assert sys.version_info >= (3, 10), "Python 3.10 or greater is required."
+
+__all__ = ["cli", "perror", "init_cli", "print_version", "HelpException"]
