4a6e14b4defc44a0fe6f6b5f0ea38648d0fe7f97
Ian Eaves <ian.k.eaves@gmail.com>
2026-01-08 01:35:29 -0600

    iterating through strategies approach
    Signed-off-by: Ian Eaves <ian.k.eaves@gmail.com>

--- ramalama/artifacts/__init__.py
+++ ramalama/artifacts/__init__.py
@@ -1,5 +1,5 @@
 # Artifact-related helpers and strategies.
 
-from ramalama.artifacts import spec, strategies, strategy
+from ramalama.artifacts import resolver, spec, strategies, strategy
 
-__all__ = ["spec", "strategy", "strategies"]
+__all__ = ["resolver", "spec", "strategy", "strategies"]
